84a1f79a1cb01e514daa6c15b6a6befd79ed7114
Yarzar <yarzar@Yarzars-MacBook-Air.local>
2024-10-22 13:00:40 +0630

    second commit assigment

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,29 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the pause button
+let pauseBtn = document.getElementById("pauseBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the pause button, open the modal
+pauseBtn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
